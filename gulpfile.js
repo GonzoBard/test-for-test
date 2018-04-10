ba611d42407ff18616bc21a09cfc8c66cacce3f2
gonzo.bard <gonzo.bard@gmail.com>
2018-04-10 22:13:59 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-// npm i --save-dev del gulp gulp-sass gulp-postcss autoprefixer gulp-concat gulp-clean-css gulp-sourcemaps run-sequence
+// npm i --save-dev gulp gulp-sass gulp-postcss autoprefixer gulp-clean-css gulp-sourcemaps gulp-rename run-sequence del
 
-const del = require('del');                         // удалить директорию, файл
 const gulp = require('gulp');
+const del = require('del');                         // удалить директорию, файл
 const autoprefixer = require('autoprefixer');       // https://github.com/postcss/autoprefixer#gulp
-const postcss = require('gulp-postcss');
-const concat = require('gulp-concat');              // собирает все файлы в один файл - делает bundle
-const sass = require('gulp-sass');
-const cleanсss = require('gulp-clean-css');         // минификация CSS
-const sourcemaps = require('gulp-sourcemaps');      // маппит bundle на исходники
+const postcss = require('gulp-postcss');            // здесь нужен для автопрефиксера
+const sass = require('gulp-sass');                  // трансляция SCSS -> CSS
+const cleancss = require('gulp-clean-css');         // минификация CSS
+const sourcemaps = require('gulp-sourcemaps');      // маппит bundle на исходники -- только для разработки
+const rename = require('gulp-rename');              // переименовать директорию, файл
 const runsequence = require('run-sequence');        // некоторые задачи надо выполнять последовательно
 
-const config = {
+const paths = {
   src: {
     fonts: './src/fonts/*.{woff2,woff}',
     html: './src/html/**/*.html',
@@ -30,45 +30,44 @@ const config = {
 };
 
 gulp.task('clean', () => {
-  return del(config.build, {force: true});
+  return del(paths.build, {force: true});
 });
 
 gulp.task('fonts', () => {
-  return gulp.src(config.src.fonts)
-    .pipe(gulp.dest(config.output.fonts));
+  return gulp.src(paths.src.fonts)
+    .pipe(gulp.dest(paths.output.fonts));
 });
 
 gulp.task('html', () => {
-  return gulp.src(config.src.html)
-    .pipe(gulp.dest(config.output.html));
+  return gulp.src(paths.src.html)
+    .pipe(gulp.dest(paths.output.html));
 });
 
 gulp.task('img', () => {
-  return gulp.src(config.src.img)
-    .pipe(gulp.dest(config.output.img));
+  return gulp.src(paths.src.img)
+    .pipe(gulp.dest(paths.output.img));
 });
 
 gulp.task('js', () => {
-  return gulp.src(config.src.js)
-    .pipe(gulp.dest(config.output.js));
+  return gulp.src(paths.src.js)
+    .pipe(gulp.dest(paths.output.js));
 });
 
 gulp.task('css', () => {
-  return gulp.src(config.src.scss)
+  return gulp.src(paths.src.scss)
   //.pipe(sourcemaps.init())
-    .pipe(sass())                                           // scss->css
-    .pipe(concat(config.output.cssName))                    // делает bundle, хотя на входе и один файл
-    .pipe(postcss([autoprefixer({                           // префиксы
+    .pipe(sass())                                 // SCSS -> CSS
+    .pipe(postcss([autoprefixer({                 // префиксы
       browsers: ['last 2 versions', 'not ie 10']
     })]))
-    .pipe(cleanсss({                                        // минификация
+    .pipe(cleancss({                              // минификация
       level: {1: {specialComments: false}}
     }))
-    //.pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.output.css));
+  //.pipe(sourcemaps.write())
+    .pipe(rename(paths.output.cssName))
+    .pipe(gulp.dest(paths.output.css));
 });
 
 gulp.task('build', () => {
   runsequence('clean', ['fonts', 'html', 'img', 'js', 'css']);
 });
-
